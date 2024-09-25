b3ce96121ac1adfe807d52ec1b7cfe65f647dfbd
Connor Mattes <clmatte@sadia.gov>
2024-09-25 15:09:42 -0600

    Generate observations and hidden in citation

--- examples/citation/citation.py
+++ examples/citation/citation.py
@@ -15,9 +15,8 @@
 
 import yaml
 import random
-import pprint
-from munch import Munch
 import pyomo.environ as pe
+import numpy as np
 
 from chmmpy import HMMBase
 from chmmpy.util import state_similarity, print_differences, run_all
@@ -49,12 +48,75 @@ class Citation(HMMBase):
         self.emission_probs = [[1-alldata.noise_prob if i == j else alldata.noise_prob/(n-1) for j in range(n)] for i in range(n)]
         self.trans_mat = [[1-alldata.transition_prob if i == j else alldata.transition_prob/(n-1) for j in range(n)] for i in range(n)]
 
+    #Oracle -- this is just for a sequence of hidden variables
+    def oracle(self, hidden):
+        for t1 in range(2,len(hidden)):
+            if(hidden[t1] != hidden[t1-1]):
+                for t2 in range(t1-1):
+                    if(hidden[t1] == hidden[t2]):
+                        return False
+        return True
+
+    
+    def generate_hidden(self, *, t_max, num_observations = 1, seed=None, debug=False):
+        H = [-1]*num_observations
+        random.seed(seed)
+        start_probs = self.start_probs
+        trans_mat = self.trans_mat
+        N = self.data.N
+        
+        #Deal with t_max
+        #I want it to either be an integer of array
+        # Check if t_max is an integer
+        if isinstance(t_max, int):
+            # Create a vector of the same length as num_observations filled with t_max
+            t_max_vector = np.full(num_observations, t_max)
+        elif isinstance(t_max, (list, np.ndarray)) and len(t_max) == num_observations:
+            # If t_max is already a vector of the correct length, use it directly
+            t_max_vector = np.array(t_max)
+        else:
+            raise ValueError("t_max must be an integer or a vector of the same length as num_observations.")
+        
+        for n in range(num_observations):
+            H[n] = [-1]*t_max_vector[n]
+            
+            #Observation at time 0
+            while True:
+                probs = [start_probs[h] for h in range(N)]
+                H[n][0] = np.random.choice(range(N), p=probs)
+                
+                for t in range(1,t_max):
+                    probs = [trans_mat[H[n][t-1]][h] for h in range(N)]
+                    H[n][t] = np.random.choice(range(N), p=probs)
+                    
+                if(self.oracle(H[n])):
+                    break
+
+        return np.array(H, dtype=int)
+
+    #Needs to be slightly different than the previous generate_observations because we want
+    #this to just depend upon a feasibly generated sequence of hidden states
+    def generate_observations_from_hidden(self, H, *, return_obs = False):  
+        emission_probs = self.emission_probs
+        N = self.data.N
+        O = [0]*len(H)
+        
+        for i in range(len(H)):
+            O[i] = [0]*len(H[i])
+            for t in range(len(H[i])):
+                probs = [emission_probs[H[i][t]][o] for o in range(N)]
+                O[i][t] = np.random.choice(range(N), p = probs)
+        
+        if(return_obs):
+            return np.array(O, dtype=int)
+        self.O = O 
+                
 
     # Generate feasible hidden states based on observations
     # Does this using a rejection sampling approach
     # Observations are a variable in HMMBase as self.O
-    def constrained_generate_from_observation(self, num = 1):
-        return 1
+    def generate_from_observation(self, *, num = 1, seed=None):
+        return -1
         
 
 
@@ -66,6 +128,8 @@ seed = None
 
 model = Citation()
 model.load_process(filename="citation.yaml")
-print(model.constrained_generate_from_observation(2))
+H = model.generate_hidden(t_max = 30, num_observations=1)
+print(H)
+print(model.generate_observations_from_hidden(H,return_obs=True))
 
 print("FINISHED")
